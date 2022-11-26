e73a06631fea74d5179dacb2150cc30ff8c19cb1
M. Lammers <60792074+martijnlammers@users.noreply.github.com>
2022-11-26 18:39:13 +0100

    feat: added page files.

---
+++ LYFTUP-src/Pages/Login.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native';
+import { InputField } from '../Components/InputField';
+
+export function Login() {
+    return (
+        <>
+            <InputField/>
+        </>
+    )
+}
